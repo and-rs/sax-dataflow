e0d9e4b969b8674f6de6c970529cc83eb5563c68
and-rs <deserd@protonmail.com>
2025-02-21 02:46:46 -0500

    tuning the nix shell

--- shell.nix
+++ shell.nix
@@ -2,13 +2,10 @@
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.bun
-    pkgs.biome
-    pkgs.nodejs_22
-
     pkgs.corepack_latest
     pkgs.pnpm-shell-completion
 
+    pkgs.nodejs_22
     pkgs.nodePackages.aws-cdk
   ];
 }
