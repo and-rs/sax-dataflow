e4c4a7593bae024bda282c1f95b933b93c61c066
and-rs <deserd@protonmail.com>
2025-02-22 14:26:08 -0500

    final node.js with nix setup

--- flake.nix
+++ flake.nix
@@ -1,23 +1,32 @@
 {
-  description = "AWS CDK Development Environment";
-
-  inputs = {
-    nixpkgs.url =
-      "github:NixOS/nixpkgs/nixos-24.11"; # Using latest before unstable
-    flake-utils.url = "github:numtide/flake-utils";
+  description = "A basic flake with node";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        # Function to create script (alias)
+        mkScript = name: text:
+          let script = pkgs.writeShellScriptBin name text;
+          in script;
+
+        # Define your scripts
+        scripts = [
+          (mkScript "cdk" ''
+            pnpx aws-cdk "$@"
+          '')
+        ];
       in {
         devShells.default = pkgs.mkShell {
-          buildInputs =
-            [ pkgs.nodejs_22 pkgs.corepack pkgs.nodePackages.aws-cdk ];
-
-          # shellHook = ''
-          #   export npm_config_unsafe_perm=true
-          # '';
+          buildInputs = [ pkgs.nodejs_22 ]
+            ++ scripts; # Adding scripts to the environment
         };
       });
 }
